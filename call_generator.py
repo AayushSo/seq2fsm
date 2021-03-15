cae41ad2dbe90f1a16c97f36f3be987007f3af16
Aayush Soni <75730831+AayushSo@users.noreply.github.com>
2021-03-15 21:27:06 +0530

    Update call_generator.py
    Add support for don't-care in sequence

--- call_generator.py
+++ call_generator.py
@@ -16,7 +16,6 @@
 ## USE THIS PROGRAM TO DIRECTLY GENERATE VERILOG CODE FROM SEQUENCE.
 ## To directly print to file add '>' along with file name in terminal :
 ## python call_generator.py -s '101_enter_the_seq_101' > 'file_to_print.txt'
-import fsm_gen as F
 
 import argparse
 
@@ -33,12 +32,17 @@ ap.add_argument('-s','--sequence', required = True, help = "Sequence of bits to 
 ap.add_argument('-ce','--clock-edge', required = False,default = 'p', help = "clock edge. 'p' for positive edge, 'n' for negative edge. Default positive edge")
 ap.add_argument('-cn','--clock', required = False,default = 'clk', help = "Name of clock signal. Default 'clk'")
 ap.add_argument('-r','--reset', required = False,default = 'rst', help = "Name of reset signal")
+ap.add_argument('-x','--include-x', action = 'store_true', help = "Include x/X/? for don't care's")
 
 args = vars(ap.parse_args())
 #print(args)
 
+if args["include_x"] : 
+	import fsm_gen_v2 as F
+else : import fsm_gen as F
 
-F.verilog(args["sequence"],enco_type=args["encoding"],input_wire=args["input_signal"],state =args["state"],
+
+try :F.verilog(args["sequence"],enco_type=args["encoding"],input_wire=args["input_signal"],state =args["state"],
 	next_state=args["next_state"],output_wire = args['output_signal'] ,clock_name = args['clock'],
 	edge = args['clock_edge'], reset = args['reset']);
-	
+except Exception : print("Got unexpected don't care. Include '-x' or '--include-x' in arguments to include don't care positions")
